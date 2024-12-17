31ee5f4458b13475f2f8fcde671f60c3338108f5
Pankaj Kumar <kuldeepydv15@gmail.com>
2024-12-17 14:57:33 +0530

    DataBaseCreated

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();  
-app.use(cors());
+app.use(cors({
+  origin: 'https://driverassesmenttest.netlify.app' // Specific frontend URL allow
+}));
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
